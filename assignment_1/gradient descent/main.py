4fc9e03f69396e358a31daa224157c5dd2b30715
Cyrille Gindreau <cyrillegin@gmail.com>
2017-09-04 15:43:18 -0600

    file reading and graph line

--- assignment_1/gradient descent/main.py
+++ assignment_1/gradient descent/main.py
@@ -1,22 +1,37 @@
 import random
 import matplotlib.pyplot as plt
+import json
 
 
 def createData():
-    data = [[], [], [], []]
-    for i in range(0, 20):
-        data[0].append(int(random.random() * 100))
-        data[1].append(int(random.random() * 100))
-        data[2].append(int(random.random() * 100))
-        data[3].append(int(random.random() * 100))
+    data = []
+    with open('fruit_data.txt', 'r') as txt:
+        for i in txt.readlines():
+            data.append(i.split())
     return data
 
 
-def drawScatterPlot(data):
-    plt.scatter(data[0], data[1], c='b')
-    plt.scatter(data[2], data[3], c='r')
+def sortData(data):
+    apples = [[], []]
+    oranges = [[], []]
+    for i in data:
+        if i[2] == 1:
+            apples[0].append(i[0])
+            apples[1].append(i[1])
+        else:
+            oranges[0].append(i[0])
+            oranges[1].append(i[1])
+    return apples, oranges
+
+
+def drawScatterPlot(apples, oranges):
+    plt.scatter(oranges[0], oranges[1], c='b')
+    plt.scatter(apples[0], apples[1], c='r')
+    plt.plot([-2, 2], [-2, 2])
     plt.show()
+
 
 if __name__ == "__main__":
     data = createData()
-    drawScatterPlot(data)
+    apples, oranges = sortData(data)
+    drawScatterPlot(apples, oranges)
