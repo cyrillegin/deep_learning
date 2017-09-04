77dfa037dd6655c6e869f7ca6f15ebdab99303c5
Cyrille Gindreau <cyrillegin@gmail.com>
2017-09-04 15:17:24 -0600

    scatter plot

--- assignment_1/gradient descent/main.py
+++ assignment_1/gradient descent/main.py
@@ -1,13 +1,22 @@
 import random
+import matplotlib.pyplot as plt
 
 
 def createData():
-    data = []
-    for i in range(0, 100):
-        data.append([int(random.random() * 100), int(random.random() * 100)])
+    data = [[], [], [], []]
+    for i in range(0, 20):
+        data[0].append(int(random.random() * 100))
+        data[1].append(int(random.random() * 100))
+        data[2].append(int(random.random() * 100))
+        data[3].append(int(random.random() * 100))
     return data
 
 
+def drawScatterPlot(data):
+    plt.scatter(data[0], data[1], c='b')
+    plt.scatter(data[2], data[3], c='r')
+    plt.show()
+
 if __name__ == "__main__":
-    i = createData()
-    print i
+    data = createData()
+    drawScatterPlot(data)
